8c0be0086b30d25d1dba0bbf75d548df5820a61e
Nabila Taufiq <nabila.taufiq@verizon.com>
2019-07-30 18:09:01 -0500

    Finished
    Added comments
    Made CSS changes
    Updated Read Me file

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Project 6
  Portfolio of projects
+
+I added a transform property on .portfolio-index .cell:hover, and scaled it up to 1.2 so that when the user hovers over the image on portfolio, it will enlargen. 
+
+I also made the corners of my buttons rounder.  The buttons will also become larger when the user hovers over it.
+
+The background color is changed.  
+
+I changed the color of the title of my portfolio and added a text shadow.
--- app.js
+++ app.js
@@ -1,36 +1,34 @@
+// Importing some node libraries to the file.
 const express = require("express");
-//const pug = require("pug");
 const data = require("./data.json");
 const bodyParser = require('body-parser');
 const app = express();  //create the app object using our imported express module
-//const projects = data.projects;
 
 
-//makes working with incoming data easier.
+//This makes working with incoming data easier.
 //turned from a string into data your application can use
 //for parsing applications
 app.use(bodyParser.urlencoded({ extended: false}));
 
-// setting up the pug.  update code in app to use pug.
 app.set('view engine', 'pug'); //set the 'view engine' value to specify the pug library.
 
-
+// We use the express.static middleware to get Express to serve the static files located in the public folder in the project root.
 app.use('/static', express.static('public'));
 
 
-// Adding routes.
-//This will show the projects in the home section.  
+///////////////////  ROUTES //////////////////////
 
+//This will display the projects in the home section.  
 app.get('/', (req, res)=> {
     res.render('index', {projects: data.projects});
 });
 
+//This will show the about page.
 app.get('/about', (req, res)=> {
     res.render('about');
 });
 
-//Dynamic routes
-
+//Dynamic project routes to render a unique project page page that will show off each project.  
 app.get('/projects/:id', (req, res)=> {
     console.log(req.params.id);
     res.render('project', {
@@ -40,31 +38,22 @@ app.get('/projects/:id', (req, res)=> {
 });
 
 
-
-//handling errors: •	In Express, you can use the next function to signal an error in your app.
-
+// catching an error 404 and forwarding to error handler
 app.use((req, res, next)=> {
-    const err = new Error('Oops! This page does not exist.');
-    err.status = 404;
-    next(err);
+    console.log("Error");
+    const err = new Error('Oops! This page does not exist.');  //creating an error object
+    err.status = 404;  // setting error status to 404
+    next(err);  // This signals errors on app
 });
 
+
+// Handling error
 app.use((err, req, res, next)=>{
    res.locals.error = err;
-
-   res.status(404);
-   //res.status(err.status);
-    res.render('error', err);
+   res.render('error', err);  //rendering the error template passing in the error data from err object.
 });
 
-// app.use((err, req, res, next) => {
-//     res.locals.error = err;
-//     if (error.status === 'undefined') {
-//         error.status = 500;
-//         res.status(error.status);
-//     }
-//     res.render('error');
-// });
+
 
 
 app.listen(3000, ()=> console.log("server is running"));
